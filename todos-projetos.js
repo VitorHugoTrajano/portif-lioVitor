650252ec2b24774aaf310fd324fa87e85d50d45e
Vitor Hugo <110561757+VitorHugoTrajano@users.noreply.github.com>
2023-04-01 12:11:53 -0300

    Add files via upload

---
+++ todos-projetos.js
@@ -0,0 +1,40 @@
+const containerProjetos = document.querySelector('#projetos__container');
+
+const projetos = [
+    {
+        imagem: 'assets/imagens/projetos-imagens/barbearia-alura.png',
+        titulo: 'Projeto Barbearia Alura',
+        tecnologia: 'HTML e CSS',
+        caracteristica: 'Site responsivo.'
+    },
+    {
+        imagem: 'assets/imagens/projetos-imagens/skate-e-camisa.png',
+        titulo: 'Projeto Camisa e Skate Alura',
+        tecnologia: 'HTML, CSS e JS',
+        caracteristica: 'Site responsivo.'
+    },
+    {
+        imagem: 'assets/imagens/projetos-imagens/projeto-alurageek.png',
+        titulo: 'Projeto Alura Geek',
+        tecnologia: 'HTML, CSS e JS',
+        caracteristica: 'Site responsivo.'
+    },
+    {
+        imagem: 'assets/imagens/projetos-imagens/aluraspace.png',
+        titulo: 'Projeto Alura Space',
+        tecnologia: 'REACT.JS',
+        caracteristica: 'Site não responsivo, apenas desktop.'
+    },
+    {
+        imagem: 'assets/imagens/projetos-imagens/projeto-olamundo.png',
+        titulo: 'Projeto Olá Mundo',
+        tecnologia: 'REACT.JS',
+        caracteristica: 'Site responsivo.'
+    },
+    {
+        imagem: 'assets/imagens/projetos-imagens/projeto-olamundo.png',
+        titulo: 'Projeto Olá Mundo',
+        tecnologia: 'REACT.JS',
+        caracteristica: 'Site responsivo.'
+    }
+]
